99638f5984888c13bf6cdda3a6d28ad6fc44441a
Eric Londer <ericlonder730@gmail.com>
2018-02-04 17:14:51 -0500

    ahhhhh

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,14 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter either an RGB Triplet or Hex value");
-        Scanner value = new Scanner(input.next());
-        int[] threeValues = new int[3];
-        if (value.next().contains(",")){
-            value.useDelimiter(",");
-            for (int x = 0; x < 3; x++){
-                threeValues[x] = value.nextInt();
-                System.out.println(threeValues[x]);
-            }
-        }
+        converter converter = new converter(input.nextLine());
+        System.out.println("RGB: " + converter.getRGB() + "HEX: " + converter.getHex());
     }
 }
--- src/com/company/converter.java
+++ src/com/company/converter.java
@@ -1,8 +1,61 @@
 package com.company;
 
+import java.util.Arrays;
+
 /**
  * Created by el693 (who is super cool and awesome) on 2/2/18.
  */
 public class converter {
+    public String RGB = "";
+    public String[] hex = new String[3];
+
+    public converter(String color){
+        if (color.contains(",")) {
+           RGB =
+            for (int x = 0;x < 3;x++){
+                hex[x] = convertToHex(RGB[x]);
+            }
+        }
+        else {
+            hex[0] = color.substring(0,2);
+            hex[1] = color.substring(2,4);
+            hex[2] = color.substring(4,6);
+            for (int x = 0;x < 3;x++){
+                RGB[x] = convertToRGB(hex[x]);
+            }
+        }
+    }
+
+    public String convertToHex(String x){
+        String[] colorValuesString = x.split("");
+        int[] values = new int[3];
+        for(int i =0; i<3; i++) {
+            values[i] = Integer.parseInt(colorValuesString[i]);
+        }
+        StringBuilder maker = new StringBuilder();
+        for(int i = 0; i < 3; i++) {
+            int temp = values[i];
+
+            int numOne = temp/16;
+            String numOneHex = Integer.toString(numOne, 16);
+            int numTwo = (temp % 16);
+            String numTwoHex = Integer.toString(numTwo, 16);
+
+            maker.append(numOneHex);
+            maker.append(numTwoHex);
+        }
+        return maker.toString();
+    }
+    public String convertToRGB(String x){
+        return Integer.toString(Integer.parseInt(x.substring(0,1),16)*16 + Integer.parseInt(x.substring(1),16));
+
+    }
+    public String getHex() {
+        return Arrays.toString(hex);
+    }
+
+    public String getRGB() {
+        return Arrays.toString(RGB);
+    }
 
 }
